b95736fed2e7b8d70726e84f6b906057673c2d71
Rahul Mali <rahuldmali4217@gmail.com>
2026-04-09 13:10:57 +0530

    14-localStorage

---
+++ 14-localStorage/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const App = () => {
+
+    // const user = localStorage.getItem('user')
+
+    // localStorage.setItem('age', 18)
+    // const age = localStorage.getItem('age')
+
+    // console.log(user);
+    // console.log(age);
+    // localStorage.removeItem('user')
+    // localStorage.removeItem('age')
+
+
+    // const user = {
+    //     username: 'Rahul',
+    //     age: 20,
+    //     city: 'Ahmedabad'
+    // }
+
+
+    // localStorage.setItem('user', JSON.stringify(user))
+
+
+    // const user = JSON.parse(localStorage.getItem('user'))
+    // console.log(user);
+
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
