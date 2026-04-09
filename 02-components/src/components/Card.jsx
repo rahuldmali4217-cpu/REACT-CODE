20cd5722ef2a1fc7c04a89d37f2e74c9b32f180d
Rahul Mali <rahuldmali4217@gmail.com>
2026-04-09 13:05:36 +0530

    02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <>
+            <div className="card">
+                <h2>Rahul Mali</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, esse.</p>
+            </div>
+        </>
+    )
+}
+
+export default Card
